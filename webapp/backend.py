de78022ced13ba3987a10d5623a4a310a9a0d9f7
Arian Pasquali <arianpasquali@gmail.com>
2021-05-16 01:56:51 +0200

    support new endpoint

--- webapp/backend.py
+++ webapp/backend.py
@@ -1,9 +1,12 @@
 import json
 import requests
 import os
+import urllib.request
+import ssl
 
 # make sure you specify .env
 ML_INFERENCE_SERVER_ENDPOINT = os.environ['ML_INFERENCE_SERVER_ENDPOINT']
+ML_INFERENCE_SERVER_ENDPOINT_SNORKEL = os.environ['ML_INFERENCE_SERVER_ENDPOINT_SNORKEL']
 ML_ZEROSHOT_INFERENCE_SERVER_ENDPOINT = os.environ['ML_ZEROSHOT_INFERENCE_SERVER_ENDPOINT']
 LABELSTUDIO_ENDPOINT = os.environ['LABELSTUDIO_ENDPOINT']
 LABELSTUDIO_API_TOKEN = os.environ['LABELSTUDIO_API_TOKEN']
@@ -23,7 +26,9 @@ def remote_zeroshot_inference_request(input_text, candidate_labels, multi_class=
 	print(payload)
 	response = requests.request("POST", ML_ZEROSHOT_INFERENCE_SERVER_ENDPOINT, json=payload, 
 					headers={"Content-Type": "application/json"})
-                    
+
+	# loading.empty()
+
 	return json.loads(response.text)
 
 def remote_inference_request(input_text, model_name):
@@ -37,6 +42,35 @@ def remote_inference_request(input_text, model_name):
 	response = requests.request("POST", ML_INFERENCE_SERVER_ENDPOINT, json=payload, headers=headers)
 
 	return json.loads(response.text)
+
+def remote_inference_request_snorkel(input_text, model_name="toxicity"):
+    
+    def allowSelfSignedHttps(allowed):
+        # bypass the server certificate verification on client side
+        if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+    data = {
+        "text":input_text
+    }
+
+    body = str.encode(json.dumps(data))
+    url = ML_INFERENCE_SERVER_ENDPOINT_SNORKEL
+    headers = {'Content-Type':'application/json'}
+    
+    req = urllib.request.Request(url, body, headers)
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        return json.loads(result)
+        
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(json.loads(error.read().decode("utf8", 'ignore')))
 
 def import_to_labelstudio(input_text, project_id, predicted_labels, predicted_scores, model_name="", multilabel=False):
 	
